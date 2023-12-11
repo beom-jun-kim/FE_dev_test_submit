ab6a1253f2287c2a326c46149355fe9a261d8def
beomjunkim <gimb52043@gmail.com>
2023-12-11 11:26:36 +0900

    콘솔 수정

--- test-01/script.js
+++ test-01/script.js
@@ -1,21 +1,22 @@
+// 숫자 생성
 function generateNumberCombinations(numbers) {
   const result = [];
 
   function generateCombinationsHelper(currentIndex, currentCombination, used) {
-      if (currentIndex === numbers.length) {
-          result.push(currentCombination.slice());
-          return;
-      }
+    if (currentIndex === numbers.length) {
+      result.push(currentCombination.slice());
+      return;
+    }
 
-      for (let i = 0; i < numbers.length; i++) {
-          if (!used[i]) {
-              currentCombination.push(numbers[i]);
-              used[i] = true;
-              generateCombinationsHelper(currentIndex + 1, currentCombination, used);
-              currentCombination.pop();
-              used[i] = false;
-          }
+    for (let i = 0; i < numbers.length; i++) {
+      if (!used[i]) {
+        currentCombination.push(numbers[i]);
+        used[i] = true;
+        generateCombinationsHelper(currentIndex + 1, currentCombination, used);
+        currentCombination.pop();
+        used[i] = false;
       }
+    }
   }
 
   generateCombinationsHelper(0, [], Array(numbers.length).fill(false));
@@ -23,27 +24,35 @@ function generateNumberCombinations(numbers) {
   return result;
 }
 
-function multiplyTwoNumbers(number1, number2) {
-  return parseInt(number1, 10) * parseInt(number2, 10);
-}
-
+// 무작위로 생성할 수
 const inputNumbers = [1, 3, 5, 7, 9];
 const combinations = generateNumberCombinations(inputNumbers);
 
+// 생성된 두 수 곱셈
+function multiplyTwoNumbers(number1, number2) {
+  return number1 * number2;
+}
+
+// 최대값 저장
 let maxProduct = -1;
 let maxNumbers = [];
 
+// 최대값 업데이트
 for (const combination of combinations) {
   for (let i = 1; i < inputNumbers.length; i++) {
-      const number1 = combination.slice(0, i).join('');
-      const number2 = combination.slice(i).join('');
-      const result = multiplyTwoNumbers(number1, number2);
+    const number1 = combination.slice(0, i).join("");
+    const number2 = combination.slice(i).join("");
+    const result = multiplyTwoNumbers(number1, number2);
 
-      if (result > maxProduct) {
-          maxProduct = result;
-          maxNumbers = [number1, number2];
-      }
+    if (result > maxProduct) {
+      maxProduct = result;
+      maxNumbers = [number1, number2];
+    }
+
+    console.log(`${number1} * ${number2} = ${result}`);
   }
 }
 
-console.log(`가장 큰 값: ${maxProduct}, 두 숫자: ${maxNumbers[0]}, ${maxNumbers[1]}`);
+console.log(
+  `${maxProduct}가 가장 큰 값이며, 두 숫자는 ${maxNumbers[0]}, ${maxNumbers[1]} 입니다`
+);
